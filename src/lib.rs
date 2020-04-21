803a4d56440ba21d29457030b42b0224c22e84e8
Antonio Feitosa <antonio.cfc@gmail.com>
2020-04-21 17:03:55 -0300

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![warn(clippy::all)]
+// source: https://en.wikipedia.org/wiki/Swatch_Internet_Time
+
+fn get_num_seconds_from_midnight() -> u32 {
+    use chrono::prelude::*;
+    let now: DateTime<Utc> = Utc::now();
+    now.num_seconds_from_midnight()
+}
+
+pub fn get_current_beats() -> u16 {
+    ((get_num_seconds_from_midnight() as f32) * (0.011_574 as f32)) as u16
+}
+
